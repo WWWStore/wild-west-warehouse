36b39c4a8128ece9b2ddca76ca54fb6bf190b1c5
Jon Struve <ocdammo3@gmail.com>
2019-11-18 13:42:02 -0600

    Baseline carousel placeholder images in

---
+++ src/components/carousel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Carousel() {
+  return (
+    <>
+      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder"></img>
+      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder2"></img>
+      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder3"></img>
+    </>
+  )
+};
