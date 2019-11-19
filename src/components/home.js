b974b157857648f8d7353178a2489a745cb38e5e
Jon Struve <ocdammo3@gmail.com>
2019-11-19 11:40:03 -0600

    Chagned categories file to categories-list

--- src/components/home.js
+++ src/components/home.js
@@ -4,11 +4,13 @@ import Carousel from './carousel';
 import BestSellers from './best-sellers';
 import Recent from './recent';
 import Footer from './footer';
+import CategoriesList from './categories-list';
 
 export default function Home() {
   return (
     <>
       <Carousel />
+      <CategoriesList />
       <BestSellers />
       <Recent />
       <Footer />
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Header from './header';
 import Home from './home';
