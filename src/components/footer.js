46d56fc6b668d242130a936bccb465b9044bb8ca
ColoSRJones <colosrjones@gmail.com>
2019-11-19 10:44:22 -0600

    footer style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/footer.scss'
 
 export default function Footer() {
   return (
     <>
+    <footer>
+      <section class="footer">
+        <p>&copy;2019 DeltaV Code</p>
+      </section>
+    </footer>
     </>
   )
 };
