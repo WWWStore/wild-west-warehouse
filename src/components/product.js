90285f7064893fe1897e64b041d2c910ccb386e0
Jon Struve <ocdammo3@gmail.com>
2019-11-19 10:30:34 -0600

    We have products appearing with product name and price in a list

--- src/components/product.js
+++ src/components/product.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import superagent from 'superagent';
 
-export default function Product() {
+export default function Product(props) {
+  let [products, setProducts] = useState(null);
+  
+  useEffect(() => {
+    superagent
+      .get(`https://wwwshop.herokuapp.com/products`)
+      .then(res => {
+        setProducts(res.body.results);
+      })
+  }, [])
+
+  if (!products) {
+    return (
+      <>
+        Loading...
+      </>
+    )
+  }
+
   return (
     <>
+      <ul>
+        {
+          products.map(product => (
+            // console.log(product)
+            <li key={product._id}>{product.name} -- ${product.price}</li>
+          ))
+        }
+      </ul>
     </>
   )
 };
