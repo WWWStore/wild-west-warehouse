b0c528bacc3b170268b8cf625dded8c026bc66f5
Jon Struve <ocdammo3@gmail.com>
2019-11-20 13:43:40 -0600

    Have valid placeholder images on the home page

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import superagent from 'superagent';
 
 export default function Carousel() {
+  let [hat, setHat] = useState(null);
+  let [revolver, setRevolver] = useState(null);
+  let [beans, setBeans] = useState(null);
+
+  useEffect(() => superagent.get('https://wwwshop.herokuapp.com/products/5dd4573a9639650004f485d6').then(res => setHat(res.body.image_url)), []);
+  useEffect(() => superagent.get('https://wwwshop.herokuapp.com/products/5dd45d169639650004f485e3').then(res => setRevolver(res.body.image_url)), []);
+  useEffect(() => superagent.get('https://wwwshop.herokuapp.com/products/5dd45d359639650004f485e6').then(res => setBeans(res.body.image_url)), []);
+
   return (
     <>
-      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder"></img>
-      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder2"></img>
-      <img src="https://via.placeholder.com/200x100.jpg" alt="Placeholder3"></img>
+      <img src={hat} height={250} alt="Placeholder"></img>
+      <img src={beans} height={250} alt="Placeholder2"></img>
+      <img src={revolver} height={250} alt="Placeholder3"></img>
     </>
   )
 };
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Carousel from './carousel';
 import BestSellers from './best-sellers';
