e3492057e9a5c580427186f19efd86b2150182bc
Ian Smith <ianmsmith17@gmail.com>
2019-11-21 14:35:29 -0600

    fixed bug where it would attempt to map over a non-existent array

--- src/components/recentlyViewed.js
+++ src/components/recentlyViewed.js
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom';
 import '../styles/recentlyViewed.scss';
 
 export default function RecentlyViewed() {
-
-  return (
-    <ul className="recentlyViewed">
-      {JSON.parse(localStorage.getItem('recentlyViewed')).map(product => (
-        <li key={product._id}><Link to={`/products/${product._id}`}>
-        <h4 className="product-name">{product.name}</h4>
-        <img className="product-image" src={product.image_url} alt={product.name}/>
-        <h5 className="product-price">${product.price}</h5>
-      </Link></li>
-    ))}
-    </ul>
-  )
+  if(localStorage.getItem('recentlyViewed')) {
+    return (
+      <ul className="recentlyViewed">
+        {JSON.parse(localStorage.getItem('recentlyViewed')).map(product => (
+          <li key={product._id}><Link to={`/products/${product._id}`}>
+          <h4 className="product-name">{product.name}</h4>
+          <img className="product-image" src={product.image_url} alt={product.name}/>
+          <h5 className="product-price">${product.price}</h5>
+        </Link></li>
+      ))}
+      </ul>
+    )
+  } else {
+    return null;
+  }
 }
---
+++ src/components/sub-heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CategoriesList from './categories-list';
+
+export default function SubHeader() {
+  return (
+    <CategoriesList />
+  )
+}
