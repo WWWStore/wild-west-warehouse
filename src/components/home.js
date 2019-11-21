1f61f0950f48a23d9c1eabebcffdd48f525a95be
Ian Smith <ianmsmith17@gmail.com>
2019-11-21 13:54:58 -0600

    added local storage and displayed them on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import BestSellers from './best-sellers';
-import Recent from './recent';
 import CategoriesList from './categories-list';
 import Footer from './fixed-footer';
+import RecentlyViewed from './recentlyViewed';
 
 export default function Home() {
   return (
     <>
       <CategoriesList />
       <BestSellers />
-      <Recent />
+      <RecentlyViewed />
       <Footer />
     </>
   )
---
+++ src/components/recentlyViewed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/recentlyViewed.scss';
+
+export default function RecentlyViewed() {
+
+  return (
+    <ul className="recentlyViewed">
+      {JSON.parse(localStorage.getItem('recentlyViewed')).map(product => (
+        <li key={product._id}><Link to={`/products/${product._id}`}>
+        <h4 className="product-name">{product.name}</h4>
+        <img className="product-image" src={product.image_url} alt={product.name}/>
+        <h5 className="product-price">${product.price}</h5>
+      </Link></li>
+    ))}
+    </ul>
+  )
+}
