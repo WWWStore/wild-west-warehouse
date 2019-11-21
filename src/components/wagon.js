b867720c4c01e61b16dee1a74bfd93c43aabacb8
Jon Struve <ocdammo3@gmail.com>
2019-11-21 11:14:26 -0600

    Added delete product from wagon functionality

--- src/components/wagon.js
+++ src/components/wagon.js
@@ -9,14 +9,25 @@ export default function Wagon() {
   const authenticatedUser = useContext(AuthContext);
   
   useEffect(() => {
+    if (!authenticatedUser.token)
+      return;
+
     superagent
       .get(`https://wwwshop.herokuapp.com/wagon`)
       .set('authorization', `Bearer ${authenticatedUser.token}`)
       .then(res => {
-        console.log(res.body);
         setWagon(res.body);
       })
-  }, [])
+  }, [authenticatedUser.token])
+
+  const deleteProduct = productId => {
+    superagent
+      .delete(`https://wwwshop.herokuapp.com/wagon/${productId}`)
+      .set('authorization', `Bearer ${authenticatedUser.token}`)
+      .then(res => {
+        setWagon(res.body)
+      })
+  }
 
   if (!wagon) {
     return (
@@ -30,16 +41,22 @@ export default function Wagon() {
     <h2>Wagon</h2>
     <ul>
       {
-        wagon.map(wagonItem => (
-          <li className="wagon-product" key={wagonItem.product._id}>
-            <h3>{wagonItem.product.name}</h3>
-            <h4>${wagonItem.product.price}</h4>
-            <img src={wagonItem.product.image_url} width={200} alt={wagonItem.product.name}/>
-            <p>{wagonItem.product.description}</p>
-            <p>{wagonItem.quantity}</p>
-            <h6>Keywords: {wagonItem.product.keywords}</h6>
-          </li>
-        ))
+        wagon.map(wagonItem => {
+          if(wagonItem.quantity && wagonItem.quantity !== 0) {
+            return (
+              <li className="wagon-product" key={wagonItem._id}>
+                <h3 className="wagon-product name">{wagonItem.product.name}</h3>
+                <h4 className="wagon-product price">${wagonItem.product.price}</h4>
+                <img className="wagon-product image" src={wagonItem.product.image_url} width={200} alt={wagonItem.product.name}/>
+                <p className="wagon-product description">{wagonItem.product.description}</p>
+                <p className="wagon-product quantity">Quantity: {wagonItem.quantity}</p>
+                <h6 className="wagon-product keywords">Keywords: {wagonItem.product.keywords}</h6>
+                <button className="wagon-product delete" name="deleteProduct" onClick={() => deleteProduct(wagonItem.product._id)}>Delete</button>
+              </li>
+            )
+          }
+          return null;
+        })
       }
     </ul>
     </> 
