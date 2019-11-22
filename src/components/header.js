a270cde3d121d9cb5c60d5c6b69b362131bb5b3e
Ian Smith <ianmsmith17@gmail.com>
2019-11-22 10:30:44 -0600

    Styled dropdown and fixed click functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,46 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../authContext';
 import '../styles/header.scss';
+const styles= require('../styles/dropdown.scss');
 
 export default function Header() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [isMenuClicked, setMenuClicked] = useState(false);
   const authenticatedUser = useContext(AuthContext);
 
-  const handleMenuClick = () => {
-    setMenuClicked(!isMenuClicked);
-    if(isMenuClicked) {
-      document.getElementById('dropdown').classList.remove('hidden');
+  const [isActive, setIsActive] = useState(false);
+  let classString = isActive ? "dropdown" : "dropdown hidden";
+
+  const handleHamburgerClick = () => {
+    if(isActive) {
+      setIsActive(false);
     } else {
-      document.getElementById('dropdown').classList.add('hidden');
+      setIsActive(true);
     }
   }
 
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  }
+  
+  const handleClick = useCallback(event => {
+    console.log(isActive)
+    if (!document.getElementsByClassName('dropdown')[0].classList.contains('hidden')) {
+      setIsActive(false);
+    }
+  }, []);
+
   useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    }
     window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
+
+    if (isActive && screenWidth <= 768) {
+      window.addEventListener('click', handleClick);
+      return () => {
+        window.removeEventListener('click', handleClick);
+        window.removeEventListener('resize', handleResize);
+      }
     }
-  })
+  });
 
   let regex = /^NONUSER-/
   let displayUser = regex.test(authenticatedUser.username) ? 'Guest' : authenticatedUser.username;
@@ -40,9 +55,9 @@ export default function Header() {
           <p className="username-display">Howdy, {displayUser}!</p>
         </div>
         <ul className="header-nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/wagon"><img className="cart-logo" src={require('../assets/wagonicon.png')} alt="Wagon Logo"/></Link></li>
+          <li><Link to="/about" className="border">About</Link></li>
+          <li><Link to="/login" className="border">Login</Link></li>
+          <li><Link to="/wagon" className="border"><img className="cart-logo" src={require('../assets/wagonicon.png')} alt="Wagon Logo"/></Link></li>
         </ul>
       </header>
     )
@@ -52,15 +67,15 @@ export default function Header() {
       <header className="header">
       <Link to="/"><img className="www-logo"
           src={require('../assets/wwwlogo.png')} alt="Home Logo"></img></Link>
-          <p className="username-display">Howdy, {displayUser}!</p>
+          
         <ul className="header-nav">
-          <li><Link to="/wagon"><img className="cart-logo" src={require('../assets/wagonicon.png')} alt="Wagon Logo"/></Link></li>
-          <li><img className="hamburger" src={require('../assets/menu-alt-512.png')} alt="Hamburger" onClick={() => {handleMenuClick()}}/></li>
+          <li><Link to="/wagon" className="mobile-wagon"><img className="cart-logo" src={require('../assets/wagonicon.png')} alt="Wagon Logo"/></Link></li>
+          <li><img className="hamburger" src={require('../assets/menu-alt-512.png')} alt="Hamburger" onClick={handleHamburgerClick}/></li>
         </ul>
       </header>
-      <ul className="hidden" id="dropdown">
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/about">About</Link></li>
+      <ul style={styles} className={classString} id="dropdown">
+        <Link to="/login"><li>Login</li></Link>
+        <Link to="/about"><li>About</li></Link>
       </ul>
     </>
   )
